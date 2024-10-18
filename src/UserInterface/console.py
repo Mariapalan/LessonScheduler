32e296b5a78bad178d98054d327520d0296dfba6
mariabalanjian <maria.balanjian@gmail.com>
2024-10-18 18:09:53 -0400

    console processings

--- src/UserInterface/console.py
+++ src/UserInterface/console.py
@@ -1,3 +1,23 @@
 class Console():
 	def __init__(self):
 		pass
+
+class Console:
+    def __init__(self):
+        
+        pass
+
+    def process_offerings_admin(self, admin):
+       
+        print(f"Processing offerings for Admin: {admin.name}")
+     
+
+    def process_offerings_instructor(self, instructor):
+       
+        print(f"Processing offerings for Instructor: {instructor.name}")
+      
+
+    def process_offerings_client(self, client):
+        
+        print(f"Processing offerings for Client: {client.name}")
+       
