17d276943896c32ee4fd7c6c80098684e3c2ee37
Omar Ziad <O.Ziad@outlook.com>
2024-10-18 17:51:10 -0400

    Create Offerings class and catalog helper methods

--- src/offerings/offering.py
+++ src/offerings/offering.py
@@ -10,3 +10,23 @@ class Offering:
             return (self.location, self.schedule) == (other.location, other.schedule)
         return False
 
+
+class Offerings:
+    offerings_catalog = []
+
+    @staticmethod
+    def isOfferingUnique(location, schedule):
+        for offering in Offerings.offerings_catalog:
+            if offering.location == location and offering.schedule == schedule:
+                return False
+        return True
+
+    def createOffering(location, schedule, lesson_type):
+        if Offerings.isOfferingUnique(location, schedule):
+            Offerings.add(Offering(location, schedule, lesson_type))
+            return True
+        return False
+
+    @staticmethod
+    def add(offering):
+        Offerings.offerings_catalog.append(offering)
