65c81b897b72b42fcb07f7536dde0ba3a0ddce8f
Omar Ziad <o.ziad@outlook.com>
2024-10-18 19:19:55 -0400

    fix formatting

--- src/Users/instructor.py
+++ src/Users/instructor.py
@@ -1,5 +1,10 @@
+from src.Users.Person import Person
+
+
 class Instructor(Person):
-    
-    def __init__(self, id: str, name: str, phone: float, specialization: str, city: str):
- 
-        super().__init__(id, name, phone,specialization, city)
+
+    def __init__(
+        self, p_id: str, name: str, phone: float, specialization: str, city: str
+    ):
+        super().__init__(p_id, name, phone)
+        self.specialization = specialization
