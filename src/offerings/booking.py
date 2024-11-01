4bce13d8101b9f76e719ad3b1de9fe89de076f8a
mariabalanjian <maria.balanjian@gmail.com>
2024-11-01 19:22:15 -0400

    completed all methods for booking class

--- src/offerings/booking.py
+++ src/offerings/booking.py
@@ -14,9 +14,6 @@ class Booking:
         self.guardian = None
         self.timeslot = None
 
-
-
-
     def make_Booking(self, client, offering, time_slot):
         if offering.check_available():
             self.client = client
@@ -28,3 +25,37 @@ class Booking:
         else:
             print("Offering is not available.")
             return False
+        
+
+    def cancel_booking(self):
+        self.status = False
+        self.offering.update_availability(True)
+        return True
+    
+    def isAvailable(self):
+        return self.status
+    
+    def get_booking_details(self):
+
+        details = (
+        f"Booking ID: {self.booking_id}\n"
+        f"Client Name: {self.client.name}\n"
+        f"Client ID: {self.client.id}\n"
+        f"Offering Location: {self.offering.get_location()}\n"
+        f"Lesson Type: {self.offering.lesson_type}\n"
+        f"Date: {self.date}\n"
+        f"Time Slot: {self.time_slot}\n"
+        f"Status: {'Active' if self.status else 'Canceled'}"
+    )
+        if self.guardian:
+            details += f"\nGuardian: {self.guardian.name} (ID: {self.guardian.id})"
+        return details
+
+    def update_status(self, new_status: bool):
+        self.status = new_status
+
+    def set_guardian(self, guardian: Client):
+        self.guardian = guardian
+
+    def change_time_slot(self, new_time_slot):
+        self.time_slot = new_time_slot
