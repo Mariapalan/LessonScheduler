627cd07a3231a52f9b24627cbd2180f41132004e
mariabalanjian <maria.balanjian@gmail.com>
2024-11-11 16:37:51 -0500

    OfferingDAO class

---
+++ src/main/java/persistence/OfferingDAO.java
@@ -0,0 +1,41 @@
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import offerings.Offering;
+
+public class OfferingDAO {
+
+    private final Connection connection;
+
+    public OfferingDAO(Connection connection) {
+        this.connection = connection;
+        createTable();
+    }
+
+    //todo: adjust table schema, placeholder for now
+    private void createTable() {
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS offerings ("
+                + "id INTEGER PRIMARY KEY, "
+                + "location TEXT NOT NULL, "
+                + "description TEXT NOT NULL, "
+                + "is_available BOOLEAN DEFAULT TRUE, "
+                + ");";
+        try (Statement stmt = connection.createStatement()) {
+            stmt.execute(createTableSQL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean store(Offering offering) {
+        String insertSQL = "INSERT INTO users (name, age) VALUES ('Alice', 30);";
+        try (Statement stmt = connection.createStatement()) {
+            stmt.executeUpdate(insertSQL);
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+}
