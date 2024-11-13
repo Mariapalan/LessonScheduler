4ccabd68e881cfdc6ec82711a57aa16b188b5090
Omar Ziad <O.Ziad@outlook.com>
2024-11-12 22:32:11 -0500

    Define placeholder DAO getter methods

--- src/main/java/persistence/UserDAO.java
+++ src/main/java/persistence/UserDAO.java
@@ -1,4 +1,21 @@
 package persistence;
 
+import users.Admin;
+import users.Client;
+import users.Instructor;
+
+import java.util.Optional;
+
 public class UserDAO {
+    public Optional<Admin> getAdminById(String adminID) {
+        return Optional.empty();
+    }
+
+    public Optional<Instructor> getInstructorById(String instructorID) {
+        return Optional.empty();
+    }
+
+    public Optional<Client> getClientById(String clientID) {
+        return Optional.empty();
+    }
 }
